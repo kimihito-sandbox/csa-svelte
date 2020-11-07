2756b2afb07a7725f499fd3ea5a1642127c075a6
kimihito <tatsurotamashiro@gmail.com>
2020-11-08 05:42:27 +0900

    Fix template
    https://github.com/snowpackjs/snowpack/discussions/1533

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,10 @@ export default app;
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-  import.meta.hot.accept();
-  import.meta.hot.dispose(() => {
+const hot = import.meta.hot
+if (hot) {
+  hot.accept();
+  hot.dispose(() => {
     app.$destroy();
   });
 }
